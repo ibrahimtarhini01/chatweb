36d90445a4950a2671f19264c95e5c76a392bd7d
Ibrahim Tarhini <ibrahimtarhini01@gmail.com>
2021-03-04 22:06:30 +0200

    added room search functionality

--- client/src/components/dashboard/SideBar/UserRooms.js
+++ client/src/components/dashboard/SideBar/UserRooms.js
@@ -2,7 +2,8 @@ import React, { useState } from 'react';
 import RoomItem from './RoomItem';
 
 const UserRooms = ({ rooms }) => {
-  const [input, setInput] = useState('');
+  const [search, setSearch] = useState(rooms);
+
   return (
     <div>
       <div className=' border-bottom border-dark py-2 px-4  '>
@@ -14,19 +15,27 @@ const UserRooms = ({ rooms }) => {
             <div className='input-group  bg-transparent  border-0 w-50'>
               <input
                 type='text'
-                className='form-control bg-transparent  border-0 '
-                id='password'
+                className='form-control bg-transparent  border-0  text-white'
+                id='search'
                 placeholder='Search...'
-                value={input}
-                onChange={(e) => setInput(e.target.value)}
-                required
+                onChange={(e) => {
+                  if (e.target.value !== '')
+                    setSearch(
+                      search.filter((room) =>
+                        room.title
+                          .toUpperCase()
+                          .includes(e.target.value.toUpperCase()),
+                      ),
+                    );
+                  else setSearch(rooms);
+                }}
               />
             </div>
           </div>
         </div>
       </div>
       <div className='overflow-auto rooms'>
-        {rooms.map((room, id) => {
+        {search.map((room, id) => {
           return <RoomItem room={room} key={id} />;
         })}
       </div>
